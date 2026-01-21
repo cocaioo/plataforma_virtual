b8afc5f5301f7963b8e11daf8d3ae87eced9ea1a
SAMUEL FURTADO FORTES <077114263@fadex.org.br>
2026-01-21 14:26:51 -0300

    feat(db): Add ubs_needs table

---
+++ alembic/versions/62c7a9704dab_add_ubs_needs_table.py
@@ -0,0 +1,43 @@
+"""Add ubs_needs table
+
+Revision ID: 62c7a9704dab
+Revises: 20260108_0003
+Create Date: 2026-01-21 14:22:46.189045
+
+"""
+
+from __future__ import annotations
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '62c7a9704dab'
+down_revision = '20260108_0003'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.create_table('ubs_needs',
+    sa.Column('id', sa.Integer(), autoincrement=True, nullable=False),
+    sa.Column('ubs_id', sa.Integer(), nullable=False),
+    sa.Column('problemas_identificados', sa.Text(), nullable=False),
+    sa.Column('necessidades_equipamentos_insumos', sa.Text(), nullable=True),
+    sa.Column('necessidades_especificas_acs', sa.Text(), nullable=True),
+    sa.Column('necessidades_infraestrutura_manutencao', sa.Text(), nullable=True),
+    sa.Column('created_at', sa.DateTime(timezone=True), server_default=sa.text('now()'), nullable=True),
+    sa.Column('updated_at', sa.DateTime(timezone=True), nullable=True),
+    sa.Column('created_by', sa.Integer(), nullable=True),
+    sa.Column('updated_by', sa.Integer(), nullable=True),
+    sa.ForeignKeyConstraint(['created_by'], ['usuarios.id'], ),
+    sa.ForeignKeyConstraint(['ubs_id'], ['ubs.id'], ondelete='CASCADE'),
+    sa.ForeignKeyConstraint(['updated_by'], ['usuarios.id'], ),
+    sa.PrimaryKeyConstraint('id'),
+    sa.UniqueConstraint('ubs_id')
+    )
+
+
+def downgrade() -> None:
+    op.drop_table('ubs_needs')
