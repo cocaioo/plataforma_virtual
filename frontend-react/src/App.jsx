18c475163a0dde54d4cfff6da706c4dd986a72f5
SamuelFortes <samuel.fortes@ufpi.edu.br>
2026-02-07 23:29:09 -0300

    feat: redirect root path to login page and remove redundant home page

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -31,7 +30,7 @@ function App() {
       <NavBar />
       <main className="bg-gray-50 min-h-screen">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Navigate to="/login" replace />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           
